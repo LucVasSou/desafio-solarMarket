0b38e590698d41bf76aca686615bf8e3a2bd0de0
Lucas <lucasbmbr@hotmail.com>
2024-02-28 23:09:29 -0300

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CategoryButtom from './Components/CategoryButtom/CategoryButtom';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/CategoryButtom/CategoryButtom.jsx
@@ -0,0 +1,22 @@
+// import { useState } from "react"
+
+// const CategoryButtom = () => {
+
+//     const [menu,setMenu] = useState("shop")
+
+//     return (
+//         <div class="dropdown">
+//             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+//                 Categorias
+//             </button>
+//             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+//                 <li><a class="dropdown-item" href="#">Todas</a></li>
+//                 <li><a class="dropdown-item" href="#">Cama</a></li>
+//                 <li><a class="dropdown-item" href="#">Mesa</a></li>
+//                 <li><a class="dropdown-item" href="#">Banho</a></li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default CategoryButtom
